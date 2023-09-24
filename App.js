55f90e09a67d074a0fb1caa4b3f28fe656ae897d
hdewani <1320203902@bitraipur.ac.in>
2023-09-24 16:45:48 +0530

    added React-Navigation

--- App.js
+++ App.js
@@ -1,16 +1,10 @@
 import { StyleSheet, Text, TouchableOpacity, View, StatusBar } from 'react-native';
-
-import tw from 'twrnc';
+import AppNavigation from './src/navigation';
+import React from 'react'
 
 // nativewind approach
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center bg-black ">
-      <TouchableOpacity className="bg-teal-500 p-3 rounded-lg shadow-md shadow-gray-400">
-        <Text className="text-black text-3xl">React Native hitika</Text>
-      </TouchableOpacity>
-
-      <StatusBar style="dark" />
-    </View>
+    <AppNavigation />
   );
 }
